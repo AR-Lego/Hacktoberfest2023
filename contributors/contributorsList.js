98c1f98d7470d90d36725b44b3757b3d7fee4499
AR-Lego <74335940+AR-Lego@users.noreply.github.com>
2023-10-18 13:43:02 -0400

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Allen R.",
+    username: "https://github.com/AR-Lego",
+  },
 ];
